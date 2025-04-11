8a4455a72cc698a94784425a9a2859d5f5f8cbd3
Raul <ragegaro@gmail.com>
2025-04-11 01:50:13 -0600

    Se agrego el posible script de cassandra  y una pequeña modificacion en un archivo

--- CadenaHotelera/obj/Debug/net8.0-windows/CadenaHotelera.AssemblyInfo.cs
+++ CadenaHotelera/obj/Debug/net8.0-windows/CadenaHotelera.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadenaHotelera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d033a683b952a6c4957ff3a930adb32447a4ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadenaHotelera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadenaHotelera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
